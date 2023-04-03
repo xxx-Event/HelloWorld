7d3856a8c0a161d7505854acb81ca73afc95afdb
xxx-Event <s2119@e-mirim.hs.kr>
2023-04-03 12:03:24 +0900

    #1 커밋커밋

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("안녕 세상아");
         }
     }
 }
